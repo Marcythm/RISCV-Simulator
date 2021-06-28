d772b2b519c60b2f9f76e0848c5d2d567819e1ad
Marcythm <marcythm@gmail.com>
2021-06-29 02:01:23 +0800

    add Instruction::Decode

--- Instruction.cpp
+++ Instruction.cpp
@@ -1,25 +1,70 @@
 #include "Instruction.hpp"
 
-auto Instruction::Decode(const u32 encoding, const u32 pc, const u32 reg[32]) -> std::unique_ptr<Instruction> {
-    switch (getbits<6, 0>(encoding)) {
-        case 0b0010011:
+#define RET(mnemonic) return std::make_unique<mnemonic>(encoding, pc, reg)
+#define CASE(mnemonic, type) case EncodingTable::mnemonic::type: RET(mnemonic)
 
-        case 0b0110111:
-            return std::make_unique<LUI>(encoding, pc, reg);
-        case 0b0010111:
-
-        case 0b0110011:
-
-        case 0b1101111:
-
-        case 0b1100111:
-
-        case 0b1100011:
-
-        case 0b0000011:
-
-        case 0b0100011:
-        ;
+auto Instruction::Decode(const u32 encoding, const u32 pc, const u32 reg[32])
+    -> std::unique_ptr<Instruction> {
+    switch (GetOpcode(encoding)) {
+    case 0b0010011:
+        switch (GetFunct3(encoding)) {
+            CASE(ADDI, funct3);
+            CASE(SLTI, funct3);
+            CASE(SLTIU, funct3);
+            CASE(ANDI, funct3);
+            CASE(ORI, funct3);
+            CASE(XORI, funct3);
+            CASE(SLLI, funct3);
+        case 0b101:
+            if (getbits<30>(encoding)) RET(SRLI);
+            else RET(SRAI);
+        }
+    CASE(LUI, opcode);
+    CASE(AUIPC, opcode);
+    case 0b0110011:
+        switch (GetFunct3(encoding)) {
+        case 0b000:
+            switch (GetFunct7(encoding)) {
+                CASE(ADD, funct7);
+                CASE(SUB, funct7);
+            }
+            CASE(SLT, funct3);
+            CASE(SLTU, funct3);
+            CASE(AND, funct3);
+            CASE(OR, funct3);
+            CASE(XOR, funct3);
+            CASE(SLL, funct3);
+        case 0b101:
+            switch (GetFunct7(encoding)) {
+                CASE(SRL, funct7);
+                CASE(SRA, funct7);
+            }
+        }
+    CASE(JAL, opcode);
+    CASE(JALR, opcode);
+    case 0b1100011:
+        switch (GetFunct3(encoding)) {
+            CASE(BEQ, funct3);
+            CASE(BNE, funct3);
+            CASE(BLT, funct3);
+            CASE(BLTU, funct3);
+            CASE(BGE, funct3);
+            CASE(BGEU, funct3);
+        }
+    case 0b0000011:
+        switch (GetFunct3(encoding)) {
+            CASE(LB, funct3);
+            CASE(LH, funct3);
+            CASE(LW, funct3);
+            CASE(LBU, funct3);
+            CASE(LHU, funct3);
+        }
+    case 0b0100011:
+        switch (GetFunct3(encoding)) {
+            CASE(SB, funct3);
+            CASE(SH, funct3);
+            CASE(SW, funct3);
+        }
     }
     return nullptr;
 }
