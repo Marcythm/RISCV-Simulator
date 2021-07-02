dd6d87313cb7928468c5fe3b6cbfc6554b05b527
Marcythm <marcythm@gmail.com>
2021-07-02 16:17:50 +0800

    add InputWire/OutputWire

---
+++ src/Wire.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "config.hpp"
+
+namespace {
+  template <u32 Width = 32u>
+  struct InputWireImpl {
+    virtual ~InputWireImpl() = default;
+    virtual auto get() const -> bits<Width> = 0;
+  };
+
+  template <u32 Width = 32u>
+  struct OutputWireImpl {
+    virtual ~OutputWireImpl() = default;
+    virtual auto set(const bits<Width> &value) -> void = 0;
+  };
+
+  template <u32 Width = 32u>
+  struct WireImpl: InputWireImpl<Width>, OutputWireImpl<Width> {
+    WireImpl() = default;
+    ~WireImpl() = default;
+    auto get() const -> bits<Width> { return _value; }
+    auto set(const bits<Width> &value) -> void { _value = value; }
+  private:
+    bits<Width> _value;
+  };
+}
+
+template <u32 Width>
+struct InputWire {
+  std::shared_ptr<InputWireImpl<Width>> impl;
+
+  InputWire(): impl(std::make_shared<WireImpl<Width>>()) {}
+  InputWire(const OutputWire<Width> &);
+  auto operator= (const OutputWire<Width> &) -> InputWire<Width>&;
+
+  auto get() const -> bits<Width> { return impl->get(); }
+  auto getWire() const -> std::shared_ptr<WireImpl<Width>> {
+    return std::dynamic_pointer_cast<WireImpl<Width>>(impl);
+  }
+};
+
+template <u32 Width>
+struct OutputWire {
+  std::shared_ptr<OutputWireImpl<Width>> impl;
+
+  OutputWire(): impl(std::make_shared<WireImpl<Width>>()) {}
+  OutputWire(const InputWire<Width> &);
+  auto operator= (const InputWire<Width> &) -> OutputWire<Width>&;
+
+  auto set(const bits<Width> &value) -> void { impl->set(value); }
+  auto getWire() const -> std::shared_ptr<WireImpl<Width>> {
+    return std::dynamic_pointer_cast<WireImpl<Width>>(impl);
+  }
+};
+
+template <u32 Width>
+InputWire<Width>::InputWire(const OutputWire<Width> &ow):
+  impl(ow.getWire()) {}
+
+template <u32 Width>
+auto InputWire<Width>::operator= (const OutputWire<Width> &ow)
+  -> InputWire<Width>& {
+  impl = ow.getWire();
+  return *this;
+}
+
+template <u32 Width>
+OutputWire<Width>::OutputWire(const InputWire<Width> &iw):
+  impl(iw.getWire()) {}
+
+template <u32 Width>
+auto OutputWire<Width>::operator= (const InputWire<Width> &iw)
+  -> OutputWire<Width>& {
+  impl = iw.getWire();
+  return *this;
+}
--- src/config.hpp
+++ src/config.hpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 
 #include <tuple>
+#include <bitset>
 #include <memory>
 #include <type_traits>
 
@@ -23,4 +24,10 @@ using u16 = unsigned short;
 using u32 = unsigned int;
 using u64 = unsigned long long;
 
+template <u32 XLEN>
+using bits = std::bitset<XLEN>;
+
 constexpr bool NOASSERT = false;
+
+template <u32 Width = 32u> struct InputWire;
+template <u32 Width = 32u> struct OutputWire;
