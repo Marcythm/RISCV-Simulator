f20ad83e02719f684593f4e8d0bf6f548f234172
Marcythm <marcythm@gmail.com>
2021-07-02 15:14:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RISC-V Simulator
+
+this is a RISC-V Simulator, supporting a subset of RV32I ISA.
+
+The purpose of this project is to simulate the behavior of the circuit in the most realistic way.
---
+++ src/config.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+#include <tuple>
+#include <memory>
+#include <type_traits>
+
+using i8 = char;
+using i16 = short;
+using i32 = int;
+using i64 = long long;
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+
+constexpr bool NOASSERT = false;
